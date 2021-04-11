b92de5ef30e90fed571e6859105945755d742275
ashokkumarblg <76480097+ashokkumarblg@users.noreply.github.com>
2021-04-11 19:55:09 +0530

    Update Transfer.java

--- src/main/java/com/db/awmd/challenge/domain/Transfer.java
+++ src/main/java/com/db/awmd/challenge/domain/Transfer.java
@@ -2,15 +2,35 @@ package com.db.awmd.challenge.domain;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
-@NoArgsConstructor
-@AllArgsConstructor
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
 @Data
 public class Transfer {	
-	private String accountFromId;	
+	
+	@NotNull
+	@NotEmpty
+	private String accountFromId;
+	
+	@NotNull
+	@NotEmpty
 	private String accountToId;	
+	
+	@NotNull
+	@Min(value = 0, message = "Initial balance must be positive.")
 	private BigDecimal amount;
+		
+	@JsonCreator
+	public Transfer(@JsonProperty("accountFromId") String accountFromId, @JsonProperty("accountToId") String accountToId, @JsonProperty("amount") BigDecimal amount) {
+	    this.accountFromId = accountFromId;
+	    this.accountToId = accountToId;
+	    this.amount = amount;
+	  }
 }
